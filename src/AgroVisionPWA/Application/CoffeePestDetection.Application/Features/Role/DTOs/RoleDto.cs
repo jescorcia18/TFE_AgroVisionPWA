e6592d4a221d334c43d645148b26a2065ce46431
JuanEscorcia <juan0118@gmail.com>
2026-06-06 10:33:11 -0500

    feat(api): HU12_Login
    Se agrega metodo y funcionalidad para Role
    
    BREAKING CHANGES: #79

---
+++ src/AgroVisionPWA/Application/CoffeePestDetection.Application/Features/Role/DTOs/RoleDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeePestDetection.Application.Features.Role.DTOs
+{
+    public class RoleDto
+    {
+        public int Id { get; set; }
+        public string Key { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
--- src/AgroVisionPWA/Infrastructure/CoffeePestDetection.Infrastructure/Services/AuthService.cs
+++ src/AgroVisionPWA/Infrastructure/CoffeePestDetection.Infrastructure/Services/AuthService.cs
@@ -1,12 +1,17 @@
-﻿using CoffeePestDetection.Application.Exceptions;
+﻿using CoffeePestDetection.Application.Commons;
+using CoffeePestDetection.Application.Exceptions;
 using CoffeePestDetection.Application.Features.Auth.DTOs;
 using CoffeePestDetection.Application.Interfaces;
+using CoffeePestDetection.Domain.Entities;
+using CoffeePestDetection.Domain.Enums;
+using CoffeePestDetection.Domain.Enums.Features.Auth;
 using CoffeePestDetection.Infrastructure.Persistence;
 using CoffeePestDetection.Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +24,7 @@ namespace CoffeePestDetection.Infrastructure.Services
 
         public AuthService(ApplicationDbContext context, JwtService jwtService)
         {
-            _context = context;        
+            _context = context;
             _jwtService = jwtService;
         }
 
@@ -43,6 +48,74 @@ namespace CoffeePestDetection.Infrastructure.Services
                 Token = token,
                 Expiration = DateTime.UtcNow.AddHours(8),
                 FullName = user.FullName
+            };
+        }
+
+        public async Task<RegisterResponseDto> RegisterAsync(RegisterRequestDto request)
+        {
+            if (await _context.Profiles.AnyAsync(x => x.Email == request.Email))
+            {
+                throw new BadRequestException("El correo ya existe.");
+            }
+
+            //  LÓGICA DE INTEGRACIÓN Y VALIDACIÓN DEL ENUM PARA ROLE
+            AuthEnum.Roles rolFinal;
+
+            if (string.IsNullOrWhiteSpace(request.Role))
+            {
+                // Si el frontend no envía un rol, asignamos el valor por defecto directamente como Enum
+                rolFinal = AuthEnum.Roles.Farmer;
+            }
+            else
+            {
+                // Si envía un rol, usamos método dinámico para transformarlo y validarlo
+                var rolValidado = EnumExtensions.GetFromDescription<AuthEnum.Roles>(request.Role);
+
+                // Si no se encuentra el rol en el sistema, disparamos el error de negocio
+                if (rolValidado == null)
+                {
+                    throw new BadRequestException($"El rol '{request.Role}' proporcionado no es válido.");
+                }
+
+                // PROTECCIÓN: Si el endpoint es público, bloquea la autocración de Administradores
+                if (rolValidado.Value == AuthEnum.Roles.Admin)
+                {
+                    throw new BadRequestException("No tienes permisos para registrar un usuario.");
+                }
+
+                rolFinal = rolValidado.Value;
+            }
+
+            // VALIDAMOS SI LA ORGANIZACION EXISTE
+            if (request.OrganizationId.HasValue)
+            {
+                var exists = await _context.Organizations.AnyAsync(x => x.Id == request.OrganizationId);
+
+                if (!exists)
+                    throw new NotFoundException("La organización no existe.");
+            }
+
+
+            var profile = new Profile
+            {
+                Id = Guid.NewGuid(),
+                FullName = request.FullName,
+                Email = request.Email,
+                PasswordHash = PasswordHasher.Hash(request.Password),
+                Role = request.Role ?? AuthEnum.Roles.Farmer.GetValue(),
+                OrganizationId = request.OrganizationId ?? Guid.Empty
+            };
+
+            _context.Profiles.Add(profile);
+
+            await _context.SaveChangesAsync();
+
+            return new RegisterResponseDto
+            {
+                Id = profile.Id,
+                FullName = profile.FullName,
+                Email = profile.Email,
+                Role = profile.Role
             };
         }
     }
